0557b26766a737b33b31b78bc867ad1cb64fb7de
Alexander Pusch <alexander.m.pusch@t-online.de>
2022-10-13 01:08:05 +0200

    Added cral.c and session.c

---
+++ src/cral.c
@@ -0,0 +1,9 @@
+/**
+ *
+ * CRude Accounting Library
+ * Alexander Maximilian Pusch (C) 2022 
+ * Licensed under the GNU GPL, please see LICENSE.md
+ *
+ */
+
+
---
+++ src/file.c
@@ -0,0 +1,13 @@
+/**
+ *
+ * CRude Accounting Library
+ *
+ * I use this mostly for my personal stuff, you are welcome to use it
+ * 
+ * (C) Alexander Maximilian Pusch, Licensed under the GNU GPLv3.
+ *
+ */
+
+#include <stdio.h>
+
+
